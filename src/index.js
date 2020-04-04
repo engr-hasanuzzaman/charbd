f2d36ee01af6fd2f3ea726a917c1caf755641fb3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-04-05 02:29:07 +0600

    Add css file and move all the stylesheet on index.js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App'
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
 
 render( <Router><App/></Router>, document.getElementById('root'));
