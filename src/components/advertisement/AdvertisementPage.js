02e5f80211f38c4483ec737f5caa4ca128d7f22b
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-04-06 02:32:05 +0600

    Move array of ads to componant state

--- src/components/advertisement/AdvertisementPage.js
+++ src/components/advertisement/AdvertisementPage.js
@@ -2,22 +2,26 @@ import React from 'react';
 import AdvertisementComponent from './AdvertisementComponent';
 
 class AdvertisementPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ads: [
+        { title: 'Get 20 discount on your favorite Cloth', 
+          description: 'Get 20 discount on your favorite Cloth', price: '200 BDT'
+        },
+        { title: 'Get 40 discount on your favorite Cloth', 
+          description: 'Get 20 discount on your favorite Cloth', price: '2000 BDT'
+        },
+        { title: 'Get 10 discount on your favorite Cloth', 
+          description: 'Get 20 discount on your favorite Cloth', price: '3500 BDT'
+        }
+      ]
+    }
+  }
   render(){
-    const ads = [
-      { title: 'Get 20 discount on your favorite Cloth', 
-        description: 'Get 20 discount on your favorite Cloth', price: '200 BDT'
-      },
-      { title: 'Get 40 discount on your favorite Cloth', 
-        description: 'Get 20 discount on your favorite Cloth', price: '2000 BDT'
-      },
-      { title: 'Get 10 discount on your favorite Cloth', 
-        description: 'Get 20 discount on your favorite Cloth', price: '3500 BDT'
-      }
-    ];
-
     return(
       <div className="container-fluid">
-        {ads.map((ad) => (
+        {this.state.ads.map((ad) => (
           <AdvertisementComponent key={ad.title} title={ad.title} description={ad.description}/>
           ))}
       </div>
