bfd5ed3c6b65b89243c10168510943e2350dae71
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-04-18 21:54:15 +0600

    Experiment with different componet material

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../layout';
+import Button from '@material-ui/core/Button';
 
 const HomePage = ()=>(
   <Layout>
-    <div className='jumbotron'>
-      <h1>Please find proper discount near to your preferred location</h1>
-    </div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+
+    <Button variant="contained" color="secondary">Default</Button>
   </Layout>
 );
 
