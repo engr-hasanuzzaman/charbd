7ba3e6cb6b853181f85f93f15660eef70f108310
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-04-20 01:12:39 +0600

    Creat basic card for showing advertisement

--- src/components/advertisement/AdvertisementComponent.js
+++ src/components/advertisement/AdvertisementComponent.js
@@ -1,17 +1,55 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Typography,
+  CardContent,
+  Button,
+  CardActions
+} from "@material-ui/core";
 
+const classes = {
+  gridList: {}
+};
 function AdvertisementComponent(props) {
+  const { title, img, description, price } = props;
   return (
-    <div className="ad-container">
-      <p className="title">{props.title}</p>
-      <p className="description">{props.description}</p>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={title}
+          height="140"
+          image={img}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
 AdvertisementComponent.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired
 };
 export default AdvertisementComponent;
--- src/components/advertisement/AdvertisementPage.js
+++ src/components/advertisement/AdvertisementPage.js
@@ -1,69 +1,75 @@
-import React from 'react';
-import AdvertisementComponent from './AdvertisementComponent';
-import Layout from '../layout';
+import React from "react";
+import AdvertisementComponent from "./AdvertisementComponent";
+import Layout from "../layout";
+import { Grid } from "@material-ui/core";
 
 class AdvertisementPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       ads: [
-        { title: 'Get 20 discount on your favorite Cloth', 
-          description: 'Get 20 discount on your favorite Cloth', price: '200 BDT'
+        {
+          title: "Get 20 discount on your favorite Cloth",
+          description: "Get 20 discount on your favorite Cloth",
+          price: "200 BDT",
+          img:
+            "https://images.all-free-download.com/images/graphiclarge/online_shopping_banner_hand_holding_card_bags_icons_6830791.jpg"
         },
-        { title: 'Get 40 discount on your favorite Cloth', 
-          description: 'Get 20 discount on your favorite Cloth', price: '2000 BDT'
+        {
+          title: "Get 40 discount on your favorite Cloth",
+          description: "Get 20 discount on your favorite Cloth",
+          price: "2000 BDT",
+          img:
+            "https://images.all-free-download.com/images/graphiclarge/online_shopping_banner_hand_holding_card_bags_icons_6830791.jpg"
         },
-        { title: 'Get 10 discount on your favorite Cloth', 
-          description: 'Get 20 discount on your favorite Cloth', price: '3500 BDT'
+        {
+          title: "Get 10 discount on your favorite Cloth",
+          description: "Get 20 discount on your favorite Cloth",
+          price: "3500 BDT",
+          img:
+            "https://images.all-free-download.com/images/graphiclarge/online_shopping_banner_hand_holding_card_bags_icons_6830791.jpg"
         }
       ],
-      title: '',
-      description: '',
-    }
+      title: "",
+      description: ""
+    };
 
     this.createNewOffer = this.createNewOffer.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  createNewOffer(event){
+  createNewOffer(event) {
     event.preventDefault();
-    const ad = {title: this.state.title, description: this.state.description };
-    this.setState((state) => {
-      return { ads: [...state.ads, ad], title: '', description: '' };
+    const ad = { title: this.state.title, description: this.state.description };
+    this.setState(state => {
+      return { ads: [...state.ads, ad], title: "", description: "" };
     });
   }
 
   handleChange(e) {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <Layout>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-8">
-              {this.state.ads.map((ad, i) => (
-                <AdvertisementComponent key={i} title={ad.title} description={ad.description}/>
-              ))}
-            </div>
-            <div className="col-4">
-              <form action="#" onChange={this.handleChange}>
-                <div className="form-group">
-                  <input type="text" className="form-control" name='title' value={this.state.title} />
-                </div>
-                <div className="form-group"></div>
-                  <input type="text" className="form-control" name='description' value={this.state.description}/>
-                <div className="form-group">
-                  <button className="btn btn-sm btn-success" type="submit" onClick={this.createNewOffer}>
-                    Create New Offer
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </Layout>  
+        <>
+          <Grid item md={2}>
+            Side Panel
+          </Grid>
+          <Grid item md={10}>
+            {this.state.ads.map((ad, i) => (
+              <AdvertisementComponent
+                key={i}
+                title={ad.title}
+                description={ad.description}
+                img={ad.img}
+                price={ad.price}
+              />
+            ))}
+          </Grid>
+        </>
+      </Layout>
     );
   }
 }
