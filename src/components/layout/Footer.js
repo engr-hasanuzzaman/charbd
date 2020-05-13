6556d49c260060b667188afaa8d6460366e5d9d6
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2020-05-13 23:33:20 +0600

    Organize footer a bit

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,12 @@ import { Box, Typography } from '@material-ui/core';
 
 const Footer = () => {
   return (
-    <Box display='flex' flexDirection='column' justifyContent='center' color='text.primary' style={{ backgroundColor: '#f5f5f5', opacity: 0.5, height: '50px' }}>
+    <Box 
+      display='flex' 
+      flexDirection='column' 
+      justifyContent='center' 
+      color='text.primary' 
+      style={{ backgroundColor: '#f5f5f5', opacity: 0.5, height: '50px' }}>
       <Typography align='center'>
           All the right are reserved by <b>@CharBd</b>
         </Typography>
